8496e1a80166e1d78ad5ff98301ddce9bcd8b7a7
rafaelxsouto@proton.me <rafaelxsouto@proton.me>
2025-08-05 07:59:16 -0300

    feat: implement form submission logic and update Input components in SignIn page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,19 +1,37 @@
+import { useState } from 'react'
 import { Input } from '../components/Input'
 import { Button } from '../components/Button'
 
 export function SingIn() {
+  const [email, setEmail] = useState('')
+  const [passoword, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
+
+    console.log(email, passoword)
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
       />
 
-      <Input required legend="senha" type="password" placeholder="Ra1@.-" />
+      <Input
+        required
+        legend="senha"
+        type="password"
+        placeholder="Ra1@.-"
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
-      <Button>Entrar</Button>
+      <Button type="submit" isLoading={isLoading}>Entrar</Button>
     </form>
   )
 }
