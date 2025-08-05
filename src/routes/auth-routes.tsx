9343e1a20ede8b11756ab9c378d7abe966542ceb
rafaelxsouto@proton.me <rafaelxsouto@proton.me>
2025-08-04 22:59:24 -0300

    feat: implement AuthLayout component and update routing to include it

--- src/routes/auth-routes.tsx
+++ src/routes/auth-routes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router'
-
+import { AuthLayout } from '../components/AuthLayout'
 import { SingIn } from '../pages/signin'
 
 export function AuthRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<SingIn />} />
+      <Route path="/" element={<AuthLayout />}>
+        <Route path="/" element={<SingIn />} />
+      </Route>
     </Routes>
   )
 }
