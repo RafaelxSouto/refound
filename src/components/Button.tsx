be27100c5b4a3b4cd6590f20c20c5d028ec537f2
rafaelxsouto@proton.me <rafaelxsouto@proton.me>
2025-08-12 10:29:29 -0300

    feat: update Button component styles and enhance Pagination integration in Dashboard

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@ const variants = {
   button: {
     base: 'h-12',
     icon: 'h-12 w-12',
-    iconSmall: 'h-10 w-10'
+    iconSmall: 'h-8 w-8',
   },
 }
 
@@ -28,7 +28,10 @@ export function Button({
       type={type}
       disabled={isLoading}
       className={classMerge([
-        'flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition easy-linear disabled:opacity-50 disable:cursor-not-allowed disabled:cursor-progress', variants.button[variant],className
+        'flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition easy-linear disabled:opacity-50 ',
+        variants.button[variant],
+        isLoading && "disabled:cursor-progress",
+        className,
       ])}
       {...rest}
     >
