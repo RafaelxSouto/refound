295ce561abe3187ea98ed77ff5b2df6d967ab546
rafaelxsouto@proton.me <rafaelxsouto@proton.me>
2025-08-12 10:03:01 -0300

    feat: add Pagination component and integrate it into Dashboard for improved navigation

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import { CATEGORIES } from '../utils/categories'
 import { formatCurrency } from '../utils/formatCurrency'
 import { Input } from '../components/Input'
 import { Button } from '../components/Button'
+import { Pagination } from '../components/Pagination'
 import { RefundItem } from '../components/RefundItem'
 
 const REFUND_EXAMPLE = {
@@ -41,15 +42,9 @@ export function Dashboard() {
 
       <div className="mt-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll">
         <RefundItem data={REFUND_EXAMPLE} />
-        <RefundItem data={REFUND_EXAMPLE} />
-        <RefundItem data={REFUND_EXAMPLE} />
-        <RefundItem data={REFUND_EXAMPLE} />
-        <RefundItem data={REFUND_EXAMPLE} />
-        <RefundItem data={REFUND_EXAMPLE} />
-        <RefundItem data={REFUND_EXAMPLE} />
-        <RefundItem data={REFUND_EXAMPLE} />
-        <RefundItem data={REFUND_EXAMPLE} />
       </div>
+
+      <Pagination current={1} total={10} />
     </div>
   )
 }
